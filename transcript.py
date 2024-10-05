fc06e3b85e25b5ac492e93794791a0a5f980ff57
MilouBrr <91068677+amigourou@users.noreply.github.com>
2024-10-05 16:07:01 +0200

    Dev/emilien (#1)
    * Update README.md

--- transcript.py
+++ transcript.py
@@ -1,61 +1,77 @@
 
 import argparse
 import os
+import logging
 import warnings
-
+import torch
 # Suppress all warnings
 warnings.filterwarnings("ignore")
 
 from pyannote.audio import Pipeline
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
-
+from tqdm import tqdm
 from utils import *
 
-HUGGING_FACE_TOKEN="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"
+def transcribe_audio(input_file_path, output_file_path, processor, num_speakers, language):
+    audio_path = convert_audio_to_wav_ffmpeg(input_file_path)
 
-# Load your Whisper model and processor
-model_name = "openai/whisper-small"
-processor = WhisperProcessor.from_pretrained(model_name)
-model = WhisperForConditionalGeneration.from_pretrained(model_name)
-
-
-def transcribe_audio(input_file_path, output_file_path):
-    print("Converting .m4a to .wav...")
-    audio_path = convert_m4a_to_wav(input_file_path)
     # Load the diarization pipeline
     pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", use_auth_token=HUGGING_FACE_TOKEN)
-
+    pipeline.to(device)
     # Diarize the audio
-    diarization = pipeline(audio_path)
+    print("Starting pipeline...")
+    logging.info(f"Starting diarization on file: {audio_path}")
+    diarization = pipeline(audio_path, num_speakers=num_speakers)
+    logging.info("Diarization complete!")
 
-    # Load the audio
+    print("Loading and normalizing the audio...")
     audio, sample_rate = load_audio(audio_path)
-    audio = audio / max(abs(audio))  # Normalize the waveform
+    audio = normalize_audio(audio)  # Normalize the waveform
 
-    transcription_segments = []
+    # Open the output file for writing at the start
+    print("Transcripting...")
 
-    # Process each segment
+  
+
+    # Process each segment as it is diarized
+    current_speaker = "SPEAKER_00"
     for turn, _, speaker in diarization.itertracks(yield_label=True):
-        start_time, end_time = turn.start, turn.end
+        # print("New turn...")
         
+        start_time, end_time = turn.start, turn.end
+
         # Extract the audio segment for the current speaker
         audio_segment = audio[int(start_time * sample_rate):int(end_time * sample_rate)]
-        
+
         # Convert to the format expected by the processor
-        inputs = processor(audio_segment, return_tensors="pt", sampling_rate=sample_rate)
+        inputs = processor(audio_segment, return_tensors="pt", sampling_rate=sample_rate, language=language)
+        inputs = {k: v.to(device) for k, v in inputs.items()}
 
         # Perform transcription
         generated_ids = model.generate(inputs["input_features"])
         transcription = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-        
-        transcription_segments.append(f"{speaker}: {transcription}")
 
-    # Save the transcription to a file
-    with open(output_file_path, "w", encoding="utf-8") as f:
-        for segment in transcription_segments:
-            f.write(segment + "\n")
+        # Write each transcription segment to the file immediately
+        if current_speaker != speaker:
+            transcription_segment = f"{speaker}: {transcription}"
+        else:
+            transcription_segment = f"{transcription}"
+
+        current_speaker = speaker
+        
+        # print(transcription_segment)  # Optional: print to see live progress
+        if os.path.exists(output_file_path):
+            mode='a'
+        else:
+            mode = 'w'
+        with open(output_file_path, mode, encoding="utf-8") as f:
+            words = transcription_segment.split()  # Split the transcription into words
+            for i in range(0, len(words), 15):  # Iterate in chunks of 15 words
+                line = ' '.join(words[i:i + 15])  # Join the words back into a single line
+                f.write(line + "\n")
 
     print(f"Transcription saved to {output_file_path}")
+
 
 if __name__ == "__main__":
     # Example usage
@@ -64,17 +80,54 @@ if __name__ == "__main__":
     parser.add_argument(
         'paths', 
         nargs='+',
-        default=r"D:\Deep learning projects\Speech2text\test_conversation.m4a",
+        default=r".",
         help="File paths or directories containing audio files to transcribe.")
+    
+    parser.add_argument(
+        'model', 
+        nargs='+',
+        default=r"medium",
+        help="Model type (chose from tiny, small, medium, large)")
+    
+    parser.add_argument(
+        'language', 
+        default=r"fr",
+        help="The prefered language")
+    
+    parser.add_argument(
+        'num_speakers', 
+        default=2,
+        help="The number of speakers")
     
     args = parser.parse_args()
     audio_files = get_audio_files(args.paths)
-
-    # input_file_path = r"D:\Deep learning projects\Speech2text\test_conversation.m4a"
-    output_file_path = r"D:\Deep learning projects\Speech2text\output_conversation.txt"
+    num_speakers = args.num_speakers
+    language = args.language
+    model = args.model
     
+
+    HUGGING_FACE_TOKEN= "" #"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"
+
+    model_types = {
+        "tiny":"openai/whisper-tiny",
+        "small":"openai/whisper-small",
+        "medium":"openai/whisper-medium",
+        "large":"openai/whisper-large-v3",
+    }
+
+    assert(model in model_types), f"The model type is not supporte, chose from {model_types.keys()}"
+    # Load your Whisper model and processor
+    model_name = model_types[model]
+    processor = WhisperProcessor.from_pretrained(model_name)
+    model = WhisperForConditionalGeneration.from_pretrained(model_name)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
     for input_file_path in audio_files:
-        print(os.path.splitext(os.path.basename(input_file_path))[0])
-        output_file_path = os.path.join(os.path.dirname(input_file_path), os.path.splitext(os.path.basename(input_file_path))[0] + "_transcript.txt")
+        segments = split_audio(input_file_path, 60)
+        output_file_path = os.path.join(os.path.dirname(input_file_path),
+                                        os.path.splitext(os.path.basename(input_file_path))[0] + "_transcript.txt")
         print(f"====Transcribing conversation {os.path.basename(input_file_path)} ....====")
-        transcribe_audio(input_file_path, output_file_path)
+        for segment in tqdm(segments) :
+            transcribe_audio(segment, output_file_path, processor, num_speakers, language)
--- utils.py
+++ utils.py
@@ -1,22 +1,74 @@
 import os
 import glob
+import subprocess
+import tempfile
+
+import numpy as np
 
 import soundfile as sf
 import librosa
 from pydub import AudioSegment
 
+def split_audio(input_file, segment_duration_sec, output_dir=os.path.join(os.path.dirname(__file__),"temp_audio_segments")):
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
 
-def convert_m4a_to_wav(input_path):
-    # Load the .m4a file using pydub and convert to .wav
-    audio = AudioSegment.from_file(input_path, format="m4a")
+    audio = AudioSegment.from_file(input_file)
+    total_duration_ms = len(audio)
+    segment_duration_ms = segment_duration_sec * 1000  # Convert to milliseconds
+    segments = []
+
+    # Save each segment as a separate .wav file
+    for i in range(0, total_duration_ms, segment_duration_ms):
+        segment = audio[i:i+segment_duration_ms]
+        segment_path = os.path.join(output_dir, f"segment_{i // 1000}_{(i + segment_duration_ms) // 1000}.wav")
+        segment.export(segment_path, format="wav")
+        segments.append(segment_path)
+
+    return segments
+
+
+def normalize_audio(audio):
+    # If the audio has multiple channels (i.e., shape (n, p)), average the channels to mix down to mono
+    if len(audio.shape) > 1:
+        audio = np.mean(audio, axis=1)  # Mix down to mono by averaging across channels
     
-    # Define the temporary .wav file path
-    wav_path = input_path.replace(".m4a", ".wav")
+    # Normalize the waveform to have values between -1 and 1
+    audio = audio / np.max(np.abs(audio))
     
-    # Export audio to .wav format
-    audio.export(wav_path, format="wav")
+    return audio
+
+def convert_audio_to_wav(input_path):
+    # Extract the file extension (e.g., .mp3, .m4a) from the input file
+    file_extension = os.path.splitext(input_path)[1][1:]  # Get the file extension without the dot
     
-    return wav_path
+    if not "wav" in file_extension:
+        # Load the audio file using pydub
+        audio = AudioSegment.from_file(input_path, format=file_extension)
+        print(audio.duration_seconds)
+        # Define the output .wav file path by replacing the original extension
+        wav_path = os.path.splitext(input_path)[0] + ".wav"
+        
+        # Export audio to .wav format
+        audio.export(wav_path, format="wav")
+        
+        return wav_path
+    
+    return input_path
+
+def convert_audio_to_wav_ffmpeg(input_path):
+    # Define the output .wav file path by replacing the original extension
+    
+    wav_path = os.path.splitext(input_path)[0] + ".wav"
+    if not "wav" in input_path:
+        print("Converting to .wav...")
+        # Use ffmpeg to convert the input file to .wav format
+        command = ['ffmpeg', '-i', input_path, wav_path]
+        subprocess.run(command, check=True)
+        
+        return wav_path
+    
+    return input_path
 
 def get_audio_files(input_paths):
     audio_files = []
